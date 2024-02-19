efaac0e85b06be81b9db492eafceb52c7f8f52c2
Adriely Floriano <adriely.floriano@azulseguros.com.br>
2024-02-18 23:03:27 -0300

    Primeiro commit!

---
+++ src/main/java/br/com/toestudando/cadastroapp/CadastroAppApplication.java
@@ -0,0 +1,14 @@
+package br.com.toestudando.cadastroapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroAppApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/br/com/toestudando/cadastroapp/CadastroAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.toestudando.cadastroapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
